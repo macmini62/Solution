93f542055cb7d61f7096a4d1a2a3467c667fc3dc
macmini62 <victorkangacha@gmail.com>
2024-03-05 02:45:27 +0300

    first commit

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -1,3 +1,9 @@
+// Question 2: Fibonacci Sequence
+// Write a program to generate the Fibonacci sequence up to 100.
+
+
 public class FibonacciSequence {
-    
+    public static void main(String[] args){
+        
+    }
 }
