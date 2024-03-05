f686910037a568f1594835c692b4e019848ff10d
macmini62 <victorkangacha@gmail.com>
2024-03-05 19:22:33 +0300

    first commit

--- PowerOfTwo.java
+++ PowerOfTwo.java
@@ -1,3 +1,30 @@
+// Question 3: Power of Two
+// Write a program that takes an integer as input and returns true if the input is a power of two.
+// Examples:
+// 8=> returns true
+// 6=> returns false
+
+
+import java.util.Scanner;
+
 public class PowerOfTwo {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        boolean bool = checkValidity(num);
+        System.out.println(bool);
+    }
+
+    public static boolean checkValidity(int num){
+        if(num%2 != 0) return false;
+
+        while(num%2 == 0){
+            num = num / 2;
+            if(num == 1) return true;
+        }
+
+        return false;
+    }
 }
