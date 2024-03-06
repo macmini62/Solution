083f38409f9b9a6cd828238a1183d21afd28ac69
macmini62 <victorkangacha@gmail.com>
2024-03-06 22:27:42 +0300

    latest update

--- CountVowels.java
+++ CountVowels.java
@@ -1,3 +1,23 @@
+// Question 6: Count Vowels
+// Write a program that counts the number of vowels in a sentence.
+// eg " Hello World " => returns 3 
+
+
 public class CountVowels {
-    
+    public static void main(String[] args) {
+         char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+         String word = "My name is Victor";
+
+         int count = 0;
+
+         for(int i = 0; i < vowels.length; i++){
+            for(int j = 0; j < word.length(); j++){
+                if(vowels[i] == word.charAt(j)){
+                    count++;
+                }
+            }
+         }
+
+         System.out.println(count);
+     }
 }
--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,3 +1,33 @@
+// Question 5: Reverse Integer
+// Write a program that takes an integer as input and returns an integer with reversed digit ordering.
+// Examples:
+// For input 500, the program should return 5.
+// For input -56, the program should return -65.
+// For input -90, the program should return -9.
+// For input 91, the program should return 19.
+
+import java.util.Scanner;
+
 public class ReverseInteger {
-    
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("input number: ");
+            int input = sc.nextInt();
+
+             int reversed = reverse(input);
+             System.out.println(reversed);
+        }
+    }
+
+    public static int reverse(int num){
+        int reversed = 0;
+
+        while(num != 0){
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        return reversed;
+    }
 }
